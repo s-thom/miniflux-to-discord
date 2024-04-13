9f8fdc4620ff3cd4fe184717c1cc46b3fcb22185
Stuart Thomson <me@sthom.kiwi>
2024-04-14 09:42:27 +1200

    Add basic server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("hello world");
+import Fastify from "fastify";
+import "dotenv/config";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/ping", () => ({ ok: true }));
+
+try {
+  await fastify.listen({
+    host: process.env.LISTEN_HOST ?? "127.0.0.1",
+    port: parseInt(process.env.LISTEN_PORT ?? "80"),
+  });
+} catch (err) {
+  fastify.log.error("Unable to start server");
+  process.exit(1);
+}
